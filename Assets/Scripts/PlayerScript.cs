23f080973ad18437080c3ff8d68bac0136499d41
alapple <133106478+alapple@users.noreply.github.com>
2023-11-09 11:15:34 +0100

    implementing Jump for player

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Input = UnityEngine.Input;
 
 public class PlayerScript : MonoBehaviour
 {
+    public Rigidbody2D myRigidbody;
+
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,11 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        bool jump = Input.GetKey("Jump");
         
+        if (jump)
+        {
+            transform.Translate(new Vector3(0, 0, 0));
+        }
     }
 }
