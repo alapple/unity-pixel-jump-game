8abb1dc9191c3a3206256bcde9079bc13234f394
alapple <133106478+alapple@users.noreply.github.com>
2025-11-19 22:37:39 +0100

    Add Spikes functionality

---
+++ Assets/Scripts/Environment/Spikes.cs
@@ -0,0 +1,25 @@
+﻿using Player;
+using UI;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Environment
+{
+    public class Spikes : MonoBehaviour
+    {
+        [SerializeField]
+        private PlayerScript player;
+        [SerializeField] 
+        private HealthBarHandler healthBarHandler;
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                player.ChangeHealth(-1);
+                healthBarHandler.HealthChanged();
+                Debug.Log("entered spikes");
+            }
+        }
+    }
+}
