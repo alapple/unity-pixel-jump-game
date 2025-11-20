03fae0e1b7b31f0db8c43c3bac860e6dd8e08482
alapple <133106478+alapple@users.noreply.github.com>
2025-11-20 21:52:04 +0100

    add background

---
+++ Assets/Scripts/Environment/BackgroundController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Environment
+{
+    public class BackgroundController : MonoBehaviour
+    {
+
+        private float _startPos, _length;
+        [SerializeField]
+        private GameObject cam;
+        [SerializeField]
+        private float parallaxFactor;
+        private void Start()
+        {
+            _startPos = transform.position.x;
+            _length = GetComponent<SpriteRenderer>().bounds.size.x;
+        }
+
+        void FixedUpdate()
+        {
+            float distance = cam.transform.position.x * parallaxFactor;
+            float movement = cam.transform.position.x * (1 - parallaxFactor);
+            transform.position = new Vector3(_startPos + distance, transform.position.y, transform.position.z);
+            if (movement > _startPos + _length) _startPos += _length;
+            else if (movement < _startPos - _length) _startPos -= _length;
+        }
+    }
+}
