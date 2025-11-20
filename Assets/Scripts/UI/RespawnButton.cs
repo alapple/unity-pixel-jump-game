a0a645eaae4a885fed4dbb83629acb0d780524cf
alapple <133106478+alapple@users.noreply.github.com>
2025-11-20 22:38:00 +0100

    add RespawnButton activating Loadingscreen

--- Assets/Scripts/UI/RespawnButton.cs
+++ Assets/Scripts/UI/RespawnButton.cs
@@ -7,11 +7,14 @@ namespace UI
     public class RespawnButton : MonoBehaviour
     {
         private Button respawnButton;
+        AsyncOperation _asyncOperation;
 
         void Awake() // Changed to Awake, good for getting component references
         {
             respawnButton = GetComponent<Button>();
             respawnButton.onClick.AddListener(OnButtonClicked);
+            _asyncOperation.allowSceneActivation = false;
+
         }
 
         private void OnButtonClicked()
@@ -19,7 +22,7 @@ namespace UI
             Debug.Log("Loading Main scene...");
             // This button's ONLY job is to load the scene.
             // The RespawnManager in the "Main" scene will handle spawning.
-            SceneManager.LoadScene("Main", LoadSceneMode.Single); 
+            _asyncOperation = SceneManager.LoadSceneAsync("LoadingScreen");
         }
 
         private void OnDestroy()
