1a787aadab4f21645f1e67f843d48ba88327d95a
Crisyn <jakob.juro@gmail.com>
2026-01-20 16:00:24 +0100

    enemy damage player

--- Assets/Scripts/Enemy/AmericanEnemy.cs
+++ Assets/Scripts/Enemy/AmericanEnemy.cs
@@ -1,98 +1,129 @@
+using System.Collections;
+
+using Player;
+
 using UnityEngine;
+
 using UnityEngine.AI;
 
+using UnityEngine.Serialization;
+
+
 public class AmericanEnemy : MonoBehaviour
+
 {
+
     public int maxHealth;
+
     public float speed;
+
     public int jumpForce;
+
     private float _currantHealth;
-    public float standardAttackDamage;
+
+    public int standardAttackDamage;
+
     public float specialAttackDamage;
+
     public float specialAttackKnockback;
+
     public LayerMask groundLayer;
+
     public Rigidbody2D body;
-    public float jumpCooldown;
+
+    public float AttackDelay;
+
     private float _nextJumpTime;
-    
+
+    public SpriteRenderer spriteRenderer;
+
+    private Vector2 BoxSize = new Vector2(1, 1);
+
+    [FormerlySerializedAs("enemyLayer")] [SerializeField]
+
+    private LayerMask playerLayer;
+
     public System.Action<float, float> OnHealthChanged;
 
+
     private void Awake()
+
     {
+
         _currantHealth = maxHealth;
+
     }
 
-    void Start()
-    {
-        _nextJumpTime = Time.time;
-    }
-
-    private void Update()
-    {
-       // Jump();
-        
-    }
-
-    bool IsGrounded() {
-        Vector2 position = transform.position;
-        Vector2 direction = Vector2.down;
-        float distance = 0.5f; 
-        Vector2 boxSize = new Vector2(0.13f, 0.5f); 
-    	
-        RaycastHit2D hit = Physics2D.BoxCast(position, boxSize, 0f, direction, distance, groundLayer);
-        
-        Color debugColor = hit.collider != null ? Color.green : Color.red;
-        Debug.DrawRay(position, direction * distance, debugColor, 0.5f);
-        
-        return hit.collider != null;   
-    }
-
-    bool StandingAtBlock()
-    {
-        Vector2 position = transform.position;
-        Vector2 directionRight = Vector2.right;
-        Vector2 directionLeft = Vector2.left;
-        float distance = 1f; 
-        Vector2 boxSize = new Vector2(1f, 1f); 
-    	
-        RaycastHit2D hitLeft = Physics2D.BoxCast(position, boxSize, 0f, directionLeft, distance, groundLayer);
-        
-        Color debugColorLeft = hitLeft.collider != null ? Color.green : Color.red;
-        Debug.DrawRay(position, directionLeft * distance, debugColorLeft, 0.5f);
-        
-        RaycastHit2D hitRight = Physics2D.BoxCast(position, boxSize, 0f, directionRight, distance, groundLayer);
-        
-        Color debugColorRight = hitRight.collider != null ? Color.green : Color.red;
-        Debug.DrawRay(position, directionRight * distance, debugColorRight, 0.5f);
-
-        if ((hitLeft.collider != null || hitRight.collider != null) && IsGrounded())
-        {
-         return true;   
-        }
-        return false;
-    }
-
-    void Jump()
-    {
-        if (StandingAtBlock())
-        {
-           if (Time.time >= _nextJumpTime)
-           {
-                Debug.LogWarning("jumping");
-           //     body.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-                _nextJumpTime = Time.time + jumpCooldown;
-           }
-        }
-    }
-    
     public void ChangeHealth(int amount)
+
     {
+
         _currantHealth = Mathf.Clamp(_currantHealth + amount, 0, maxHealth);
+
         OnHealthChanged?.Invoke(_currantHealth, maxHealth);
+
         if (_currantHealth == 0)
+
         {
+
+            spriteRenderer.color = new Color(1f, 1f, 1f, 0f);
+
+            standardAttackDamage = 0;
+
+            specialAttackDamage = 0;
+
         }
-        
+
         Debug.Log(_currantHealth);
+
     }
+
+    private IEnumerator HitDetectionWindow()
+
+    {
+
+        float elapsed = 0f;
+
+
+        while (elapsed < AttackDelay)
+
+        {
+
+            float direction = Mathf.Sign(transform.lossyScale.x);
+
+            Vector2 flippedOffset = new Vector2(BoxSize.x * direction, 0);
+
+            Vector2 scanPosition = (Vector2)transform.position + flippedOffset;
+
+
+            Collider2D[] hitEnemies = Physics2D.OverlapBoxAll(scanPosition, BoxSize, 0f, playerLayer);
+
+
+            foreach (Collider2D enemy in hitEnemies)
+
+            {
+
+                if (enemy.TryGetComponent<PlayerScript>(out var target))
+
+                {
+
+                    target.ChangeHealth(-standardAttackDamage);
+
+                    Debug.Log($"Hit {enemy.name}!");
+
+                    yield break;
+
+                }
+
+            }
+
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+
+        }
+
+    }
+
 }
